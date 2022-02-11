6a2547f4b64d8e011d35cf06b9daf2049a256f23
Legionite <46542779+LegioniteTerraTech@users.noreply.github.com>
2022-02-10 14:30:21 -1000

    save help

--- TweakTech/TweakTech/KickStart.cs
+++ TweakTech/TweakTech/KickStart.cs
@@ -55,7 +55,7 @@ namespace TweakTech
             enabledMod = new OptionToggle("Enable TweakTech", Tweakables, EnableThis);
             enabledMod.onValueSaved.AddListener(() => {
                 EnableThis = enabledMod.SavedValue;
-                thisModConfig.WriteConfigJsonFile();
+                
                 if (EnableThis)
                     FDBookmark.EnableAll();
                 else
@@ -64,20 +64,20 @@ namespace TweakTech
             maxRestrict = new OptionToggle("Lock to Max Restriction", Tweakables, ChangePatcher.MaximumFireRateAdjust);
             maxRestrict.onValueSaved.AddListener(() => {
                 ChangePatcher.MaximumFireRateAdjust = maxRestrict.SavedValue;
-                thisModConfig.WriteConfigJsonFile();
+                
                 if (EnableThis)
                     FDBookmark.EnableAll();
             });
             reduceProjectilesThreshold = new OptionRange("Maximum Firerate to Reduce", Tweakables, ChangePatcher.FirerateReductionMin, 0, 6);
             reduceProjectilesThreshold.onValueSaved.AddListener(() => {
                 ChangePatcher.FirerateReductionMin = reduceProjectilesThreshold.SavedValue;
-                thisModConfig.WriteConfigJsonFile();
+                
                 if (EnableThis)
                     FDBookmark.EnableAll();
             });
             reduceProjectiles = new OptionRange("Above Maximum Cooldown Multiplier", Tweakables, ChangePatcher.ProjectileReduction, 1, 8);
             reduceProjectiles.onValueSaved.AddListener(() => { ChangePatcher.ProjectileReduction = reduceProjectiles.SavedValue; 
-                thisModConfig.WriteConfigJsonFile();
+                
                 if (EnableThis)
                     FDBookmark.EnableAll();
             });
@@ -85,7 +85,7 @@ namespace TweakTech
             blockHP150 = new OptionToggle("(SP Only) Disable 1.5 Health Multiplier", Tweakables, !ChangePatcher.UseGlobalHealthMulti);
             blockHP150.onValueSaved.AddListener(() => {
                 ChangePatcher.UseGlobalHealthMulti = !blockHP150.SavedValue;
-                thisModConfig.WriteConfigJsonFile();
+                
                 if (EnableThis)
                      FDBookmark.EnableAll();
             });
@@ -93,8 +93,9 @@ namespace TweakTech
             multiHP = new OptionRange("Block Health Muliplier [0.5 - 2.5] \n- Requires World Restart", Tweakables, ChangePatcher.GlobalHealthMulti, 0.5f, 2.5f, 0.5f);
             multiHP.onValueSaved.AddListener(() => {
                 ChangePatcher.GlobalHealthMulti = multiHP.SavedValue;
-                thisModConfig.WriteConfigJsonFile();
+                
             });*/
+            NativeOptionsMod.onOptionsSaved.AddListener(() => { thisModConfig.WriteConfigJsonFile(); });
         }
 
         public static bool LookForMod(string name)
